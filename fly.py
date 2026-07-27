7e44bf6c1e256487292727db01d20199cef3c75d
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-07-26 20:55:25 -0400

    Adjust time range for API data processing

--- fly.py
+++ fly.py
@@ -131,8 +131,8 @@ async def get_events() -> list[Event]:
 
         API_FILE.write(api_data)
 
-    start_dt = now.delta(hours=-3)
-    end_dt = now.delta(minutes=30)
+    start_dt = now.delta(hours=-6)
+    end_dt = now.delta(minutes=1)
 
     for event_group in api_data:
         if not all(
