3f2e8f2b5ebb4a16bc99aa727f6c17c71ef4848b
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-09-04 20:35:20 -0400

    Update ozog.py

--- ozog.py
+++ ozog.py
@@ -21,7 +21,7 @@ CACHE_FILE = Cache(TAG, exp=28_800)
 BASE_URL = "https://gozo.st/"
 
 REFERER = "https://unxer123.gozo.st/"
-ORIGIN = "https://unxer123.gozo.st/"
+ORIGIN = "https://unxer123.gozo.st"
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36"
 
 
