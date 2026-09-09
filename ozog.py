9680d42235f6dc1f34b37ba3ff5f5b9f2e048f59
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-09-09 17:12:37 -0400

    Update BASE_URL to new streaming site

--- ozog.py
+++ ozog.py
@@ -22,7 +22,7 @@ TAG = "OZOG"
 
 CACHE_FILE = Cache(TAG, exp=28_800)
 
-BASE_URL = "https://gozowatch.top/updates"
+BASE_URL = "ttps://ovostream.net/"
 #"https://gozo.st/"
 
 REFERER = "https://unxer123.gozo.zip/"
