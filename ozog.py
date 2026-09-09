1a8b5593e3a98991bbb88be161e5bd17de746e81
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-09-09 16:12:20 -0400

    Update ozog.py

--- ozog.py
+++ ozog.py
@@ -22,7 +22,8 @@ TAG = "OZOG"
 
 CACHE_FILE = Cache(TAG, exp=28_800)
 
-BASE_URL = "https://gozo.st/"
+BASE_URL = "https://ovostream.net/"
+#"https://gozo.st/"
 
 REFERER = "https://unxer123.gozo.zip/"
 ORIGIN = "https://unxer123.gozo.zip"
