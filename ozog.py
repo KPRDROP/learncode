1b5897262eff84d04a52f7d80b5ef51be03f097b
kprdrop <77870123+KPRDROP@users.noreply.github.com>
2026-09-10 14:19:11 -0400

    Update ozog.py

--- ozog.py
+++ ozog.py
@@ -6,7 +6,7 @@ from urllib.parse import quote, urljoin
 
 from selectolax.lexbor import LexborHTMLParser as HTMLParser
 
-from .utils import Cache, Event, Time, get_logger, leagues, network
+from utils import Cache, Event, Time, get_logger, leagues, network
 
 log = get_logger(__name__)
 
